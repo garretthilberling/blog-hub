b30d6984505fe8eb92776e1a857dd4d2972f68d0
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-27 03:09:47 -0400

    started working on front end, installed tailwind npm package

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id','title', 'post_body', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
--- DROP DATABASE IF EXISTS blog_hub_db;
+DROP DATABASE IF EXISTS blog_hub_db;
 
--- CREATE DATABASE blog_hub_db;
+CREATE DATABASE blog_hub_db;
 
 -- USE blog_hub_db;
 
