22581448a20e463d5a5ccd7ef32f0ba8d2435346
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-30 01:52:45 -0400

    styled comments partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./views/layouts/main.handlebars", "./views/partials/post-info.handlebars", "./views/dashboard.handlebars", "./views/edit-post.handlebars", "./views/login-handlebars", "single-post.handlebars"],
+  content: ["./views/layouts/main.handlebars", "./views/partials/post-info.handlebars", "./views/dashboard.handlebars", "./views/edit-post.handlebars", "./views/login-handlebars", "single-post.handlebars", "./views/partials/comments.handlebars"],
   theme: {
     extend: {},
   },
